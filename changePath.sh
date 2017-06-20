01dba82565c7f0541acd1f34ca473348b4c1e588
swbsin <swbsin@163.com>
2017-06-20 23:06:52 +0800

    add commit

---
+++ changePath.sh
@@ -0,0 +1,7 @@
+mv homepage/2017  2017/
+mv homepage/archives archives/
+mv homepage/css css/
+mv homepage/fonts fonts/
+mv homepage/index.html index.html
+mv homepage/js  js/
+mv homepage/public public/
